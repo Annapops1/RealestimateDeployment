e633629662c9214ebe3080bdbdbc3b2753b2ee9f
Annapops1 <77141496+Annapops1@users.noreply.github.com>
2025-04-07 12:19:44 +0530

    7

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,12 @@
+import os
 from app import create_app
 
+# Set environment variables
+os.environ['FLASK_APP'] = 'wsgi.py'
+os.environ['FLASK_ENV'] = 'production'
+
+# Create the application
 app = create_app()
 
 if __name__ == "__main__":
-    app.run() 
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000))) 
